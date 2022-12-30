b049191555f05642712274f49ed60cf374551442
Abdoul Sadikou <sadikouhak@gmail.com>
2022-12-29 21:40:23 -0500

    67

---
+++ nodejs-udemy/README.md
@@ -0,0 +1,7 @@
+Request & Response Headers
+On both requests and responses, Http headers are added to transport metadata from A to B.
+
+The following article provides a great overview of available headers and their role: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+
+Whilst this article is a great resource, especially to dive deeper, please don't learn this list by heart though! You'll encounter many of these headers throughout the course and I'll explain them when we need them.
+
---
+++ nodejs-udemy/app.js
@@ -0,0 +1,55 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const PORT = 3000
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+
+app.listen(PORT )
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url
+//     const method = req.method
+
+//     if( url === '/' ) {
+//         res.setHeader('Content-Type', 'text/html')
+//         res.write('<html><head><title>Main page</title></head><body><h1>Main page.</h1></body>')
+//         res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></input></form>')
+//         res.write('</html>')
+//         return res.end()
+//     }
+//     else if (url === '/users') {
+//         res.setHeader('Content-Type', 'text/html')
+//         res.write('<html>')
+//         res.write('<head><title>Users</title></head>')
+//         res.write('<body><ul><li>User 1</li><li>User2</li></ul></body>')
+//         res.write('</html>')
+//         return res.end()
+//     }
+
+//     if(url === '/create-user' && method==="POST") {
+//         const nameUser = []
+//         req.on('data', (chunk) => {
+//             nameUser.push(chunk)
+//         })
+
+//         req.on('end', () => {
+//             const nameUserParsed = Buffer.concat(nameUser).toString().split('=')[1]
+//             console.log(nameUserParsed)
+//         })
+//         res.statusCode = 302
+//         res.setHeader('Location', '/')
+//         return res.end()
+//     }
+// })
+
+// server.listen(PORT)
